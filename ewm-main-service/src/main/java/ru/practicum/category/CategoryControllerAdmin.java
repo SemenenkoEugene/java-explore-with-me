522d386ef3471abf7b6bc24a5342d3195e10227b
SemenenkoEugene <gks-08@mail.ru>
2023-10-17 11:16:16 +0300

    init

--- ewm-main-service/src/main/java/ru/practicum/category/CategoryControllerAdmin.java
+++ ewm-main-service/src/main/java/ru/practicum/category/CategoryControllerAdmin.java
@@ -1,6 +1,7 @@
 package ru.practicum.category;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,24 +10,28 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping(path = "/admin/categories")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryControllerAdmin {
     private final CategoryService categoryService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto create(@Valid @RequestBody CategoryDto categoryDto) {
+        log.debug("Получен POST запрос на добавление категории {}", categoryDto.toString());
         return categoryService.create(categoryDto);
     }
 
     @PatchMapping("/{catId}")
     public CategoryDto patch(@PathVariable long catId,
                              @Valid @RequestBody CategoryDto categoryDto) {
+        log.debug("Получен PATCH запрос на обновление категории {} c ID {}", categoryDto.toString(), catId);
         return categoryService.patch(catId, categoryDto);
     }
 
     @DeleteMapping("/{catId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable long catId) {
+        log.debug("Получен DELETE запрос для категории c ID {}", catId);
         categoryService.delete(catId);
     }
 }
--- ewm-main-service/src/main/java/ru/practicum/category/CategoryControllerPublic.java
+++ ewm-main-service/src/main/java/ru/practicum/category/CategoryControllerPublic.java
@@ -1,6 +1,7 @@
 package ru.practicum.category;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -10,17 +11,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryControllerPublic {
     private final CategoryService categoryService;
 
     @GetMapping
     public List<CategoryDto> getAll(@Valid @RequestParam(defaultValue = "0") @Min(0) int from,
                                     @Valid @RequestParam(defaultValue = "10") @Min(1) int size) {
+        log.debug("Получен GET запрос на просмотр категорий");
         return categoryService.getAll(from, size);
     }
 
     @GetMapping("/{catId}")
     public CategoryDto getById(@PathVariable long catId) {
+        log.debug("Получен GET запрос на просмотр категории по ID {}", catId);
         return categoryService.getById(catId);
     }
 }
