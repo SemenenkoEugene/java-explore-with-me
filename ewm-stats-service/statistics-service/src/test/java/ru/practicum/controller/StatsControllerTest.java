b8f8f749a87e35824e176fc573d2493fa48c5fcf
SemenenkoEugene <gks-08@mail.ru>
2023-11-06 15:47:58 +0300

    init: добавить тесты

--- ewm-stats-service/statistics-service/src/test/java/ru/practicum/controller/StatsControllerTest.java
+++ ewm-stats-service/statistics-service/src/test/java/ru/practicum/controller/StatsControllerTest.java
@@ -115,6 +115,21 @@ class StatsControllerTest {
         verifyNoMoreInteractions(statsService);
     }
 
+    @Test
+    void createEndpointHit_blankUri() throws Exception {
+        EndpointHitDto endpointHitDto = getValidEndpointHitDto();
+        endpointHitDto.setUri("     ");
+
+        mockMvc.perform(post("/hit")
+                        .content(objectMapper.writeValueAsString(endpointHitDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+
+        verifyNoMoreInteractions(statsService);
+    }
+
     private EndpointHitDto getValidEndpointHitDto() {
         return EndpointHitDto.builder()
                 .app("TestApp")
