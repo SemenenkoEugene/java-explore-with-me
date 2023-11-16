798190feec2bba09f2b44d7d3432b0e39a7068b0
SemenenkoEugene <gks-08@mail.ru>
2023-11-16 08:38:44 +0300

    init: добавить тесты

---
+++ ewm-main-service/src/test/java/ru/practicum/participationRequest/ParticipationRequestJsonTest.java
@@ -0,0 +1,35 @@
+package ru.practicum.participationRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ParticipationRequestJsonTest {
+
+    @Autowired
+    private JacksonTester<ParticipationRequestDto> participationRequestDtoJacksonTester;
+
+    @Test
+    void participationRequestDtoTest() throws IOException {
+        ParticipationRequestDto participationRequestDto = ParticipationRequestDto.builder()
+                .id(1L)
+                .requester(10L)
+                .event(100L)
+                .status(ParticipationRequestState.CONFIRMED)
+                .build();
+
+        JsonContent<ParticipationRequestDto> jsonContent = participationRequestDtoJacksonTester.write(participationRequestDto);
+
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.requester").isEqualTo(10);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.event").isEqualTo(100);
+        assertThat(jsonContent).extractingJsonPathStringValue("$.status").isEqualTo("CONFIRMED");
+    }
+}
