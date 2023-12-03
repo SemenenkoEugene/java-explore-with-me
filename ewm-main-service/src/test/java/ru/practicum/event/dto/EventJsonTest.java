4a325fdffe12615cc1cda432a7b2f6449c0af543
SemenenkoEugene <gks-08@mail.ru>
2023-12-03 15:38:50 +0300

    init: добавить тесты

---
+++ ewm-main-service/src/test/java/ru/practicum/event/dto/EventJsonTest.java
@@ -0,0 +1,114 @@
+package ru.practicum.event.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.category.CategoryDto;
+import ru.practicum.event.EventRequestStatusUpdateRequest;
+import ru.practicum.event.EventState;
+import ru.practicum.event.EventUpdateAdminRequest;
+import ru.practicum.event.EventUpdateUserRequest;
+import ru.practicum.location.LocationDto;
+import ru.practicum.user.UserShortDto;
+import ru.practicum.util.ConstantsDate;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class EventJsonTest {
+
+    private final LocalDateTime eventDateTimestamp = LocalDateTime.now();
+    private final LocalDateTime createdOnTimestamp = LocalDateTime.now().plusDays(1);
+    private final LocalDateTime publishedOnTimestamp = LocalDateTime.now().plusDays(2);
+
+    @Autowired
+    private JacksonTester<EventFullDto> eventFullDtoJacksonTester;
+
+    @Autowired
+    private JacksonTester<EventNewDto> eventNewDtoJacksonTester;
+
+    @Autowired
+    private JacksonTester<EventRequestStatusUpdateRequest> eventRequestStatusUpdateRequestJacksonTester;
+
+    @Autowired
+    private JacksonTester<EventShortDto> eventShortDtoJacksonTester;
+
+    @Autowired
+    private JacksonTester<EventUpdateAdminRequest> eventUpdateAdminRequestJacksonTester;
+
+    @Autowired
+    private JacksonTester<EventUpdateUserRequest> eventUpdateUserRequestJacksonTester;
+
+    @Test
+    void eventFullDtoTest() throws IOException {
+        EventFullDto eventFullDto = EventFullDto.builder()
+                .id(1L)
+                .initiator(getUserShortDto())
+                .category(getCategoryDto())
+                .location(getLocationDto())
+                .title("TestTitle")
+                .annotation("TestAnnotation")
+                .description("TestDescr")
+                .state(EventState.PENDING)
+                .eventDate(eventDateTimestamp)
+                .createdOn(createdOnTimestamp)
+                .publishedOn(publishedOnTimestamp)
+                .participantLimit(15)
+                .paid(true)
+                .requestModeration(false)
+                .confirmedRequests(150L)
+                .views(1500L)
+                .build();
+
+        JsonContent<EventFullDto> jsonContent = eventFullDtoJacksonTester.write(eventFullDto);
+
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.initiator.id").isEqualTo(10);
+        assertThat(jsonContent).extractingJsonPathStringValue("$.initiator.name").isEqualTo("TestUser");
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.category.id").isEqualTo(100);
+        assertThat(jsonContent).extractingJsonPathStringValue("$.category.name").isEqualTo("TestCategory");
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.location.lat").isEqualTo(1000.0);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.location.lon").isEqualTo(10000.0);
+        assertThat(jsonContent).extractingJsonPathStringValue("$.title").isEqualTo("TestTitle");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.annotation").isEqualTo("TestAnnotation");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.description").isEqualTo("TestDescr");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.state").isEqualTo("PENDING");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.eventDate")
+                .isEqualTo(eventDateTimestamp.format(ConstantsDate.getDefaultDateTimeFormatter()));
+        assertThat(jsonContent).extractingJsonPathStringValue("$.createdOn")
+                .isEqualTo(createdOnTimestamp.format(ConstantsDate.getDefaultDateTimeFormatter()));
+        assertThat(jsonContent).extractingJsonPathStringValue("$.publishedOn")
+                .isEqualTo(publishedOnTimestamp.format(ConstantsDate.getDefaultDateTimeFormatter()));
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.participantLimit").isEqualTo(15);
+        assertThat(jsonContent).extractingJsonPathBooleanValue("$.paid").isEqualTo(true);
+        assertThat(jsonContent).extractingJsonPathBooleanValue("$.requestModeration").isEqualTo(false);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.confirmedRequests").isEqualTo(150);
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.views").isEqualTo(1500);
+    }
+
+    private LocationDto getLocationDto() {
+        return LocationDto.builder()
+                .lat(1000f)
+                .lon(10000f)
+                .build();
+    }
+
+    private CategoryDto getCategoryDto() {
+        return CategoryDto.builder()
+                .id(100L)
+                .name("TestCategory")
+                .build();
+    }
+
+    private UserShortDto getUserShortDto() {
+        return UserShortDto.builder()
+                .id(10L)
+                .name("TestUser")
+                .build();
+    }
+}
