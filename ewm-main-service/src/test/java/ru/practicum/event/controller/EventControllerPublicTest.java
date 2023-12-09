c2b51c48d34ad096c3efb704bd6d71344b8784b6
SemenenkoEugene <gks-08@mail.ru>
2023-12-09 18:52:17 +0300

    init: добавить тесты

---
+++ ewm-main-service/src/test/java/ru/practicum/event/controller/EventControllerPublicTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.event.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.event.service.EventServiceImpl;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = EventControllerPublic.class)
+class EventControllerPublicTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EventServiceImpl eventService;
+
+    @Test
+    public void getAll_allValid() throws Exception {
+        when(eventService.getAllPublic(
+                any(), any(), any(), any(), any(), anyBoolean(), any(), anyInt(), anyInt(), any()
+        )).thenReturn(null);
+
+        mockMvc.perform(get("/events"))
+                .andExpect(status().is2xxSuccessful());
+
+        verify(eventService, times(1)).getAllPublic(
+                any(), any(), any(), any(), any(), anyBoolean(), any(), anyInt(), anyInt(), any()
+        );
+        verifyNoMoreInteractions(eventService);
+    }
+}
