0ab07ddb9389bb5ab795472845cbe0fe1cb1faa4
SemenenkoEugene <gks-08@mail.ru>
2023-12-13 21:32:59 +0300

    init: добавить тесты

--- ewm-main-service/src/test/java/ru/practicum/event/controller/EventControllerPrivateTest.java
+++ ewm-main-service/src/test/java/ru/practicum/event/controller/EventControllerPrivateTest.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.event.dto.EventNewDto;
 import ru.practicum.event.service.EventServiceImpl;
+import ru.practicum.location.LocationDto;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = EventControllerPrivate.class)
@@ -54,5 +61,38 @@ class EventControllerPrivateTest {
 
         verify(eventService, times(1)).getParticipationRequestsByInitiator(anyLong(), anyLong());
         verifyNoMoreInteractions(eventService);
+    }
+
+    @Test
+    public void create_allValid() throws Exception {
+        when(eventService.create(anyLong(), any())).thenReturn(null);
+
+        mockMvc.perform(post("/users/{userId}/events", 0)
+                        .content(objectMapper.writeValueAsString(getValidEventNewDto()))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+
+        verify(eventService, times(1)).create(anyLong(), any());
+        verifyNoMoreInteractions(eventService);
+    }
+
+    private EventNewDto getValidEventNewDto() {
+        return EventNewDto.builder()
+                .category(0L)
+                .location(getValidLocationDto())
+                .title("Test title")
+                .annotation("Test annotation with 20 min symbols")
+                .description("Test description with 20 min symbols")
+                .eventTimestamp(LocalDateTime.now().plusDays(1))
+                .build();
+    }
+
+    private LocationDto getValidLocationDto() {
+        return LocationDto.builder()
+                .lat(0f)
+                .lon(0f)
+                .build();
     }
 }
